e4013c8a54360724940a78267a4fd7b697555221
tedirland <irlandth@gmail.com>
2023-10-10 21:59:02 -0500

    refactor some code, hook up quest system

--- battle_engine.py
+++ battle_engine.py
@@ -1,59 +1,82 @@
 import random
+from time import sleep
 from colorama import Fore, Back, Style
 from game_data import enemies, the_hero
 from quests import quests
 
-enemies = enemies
-the_hero = the_hero
+
+
+
+
 
 def battle_engine():
-    random_index = random.randint(0,len(enemies)-1)
-    # we need to use the copy() method so we don't change the original
-    enemy_encountered = enemies[random_index].copy()
-    in_fight = True #boolean that keeps us in the fight loop
-    print(f"{enemy_encountered['intro']}")
-    print(f"A {enemy_encountered['adjective']} {enemy_encountered['name']} appears...")
-    while in_fight:
-        
-        enemy_attack_modifier = 1
-        print(f"You have {the_hero['hp']} health points remaining \n The {enemy_encountered['name']} has {enemy_encountered['hp']}")
-        print(f"1. Attack using {the_hero['weapon']}")
-        health_potion_count = the_hero['inventory'].count('health_potion')
-        print(f"2. Drink a health potion. ({health_potion_count} in your satchel)")
-        print("3. Defend")
-        print("4. Do a little dance")
-        print("5. Run away")
-        battle_action = input("What would you like to do? >")
-    # Hero's turn
-        if battle_action == "1":
-            # user has chosen to attach
-            # take the hero's attack power - monster'd defense
-            # reduce the monster's hp by this calcuation
-            damage_dealt = the_hero['attack_power'] - enemy_encountered['defense']
-            print(Fore.GREEN + f"You strike the {enemy_encountered['name']} for {damage_dealt} damage")
-            enemy_encountered['hp'] -= damage_dealt
-            # Enemy's turn, if alive
-            if enemy_encountered['hp'] > 0:
-                print("Your foe staggers but remains in the fight")
-                damage_dealt = (enemy_encountered['attack_power'] * enemy_attack_modifier) - the_hero['defense']
-                print(f"The {enemy_encountered['name']} strikes for {damage_dealt} damage")
-                the_hero['hp'] -= damage_dealt
+    # References to properties in hero dict
+    # (should the_hero be his own file. Yes, will be a class eventually)
+    h_atk_pow = the_hero['attack_power']
+    h_hp = the_hero['hp']
+    h_wpn = the_hero['weapon']
+    h_xp = the_hero['xp']
+    h_gold = the_hero['gold']
+    health_potion_count = the_hero['inventory'].count('health_potion')
 
-            else:
-                print("Your blade crunches through flesh and bone. The beast lies still and does not rise")
-                the_hero['xp'] += enemy_encountered['xp_drop']
-                the_hero['gold'] += enemy_encountered['gold_drop']
-                print(f"You earn {enemy_encountered['xp_drop']} experience and collect {enemy_encountered['gold_drop']} gold")
-                in_fight = False
-        elif battle_action == "2":
-            # Drink a potion
-            if health_potion_count > 0:
-                the_hero['inventory'].remove("health_potion")
-                the_hero['hp'] = the_hero['max_hp']
-                print("You take a health potion from your satchel and drink it greedily, restoring your lost hp")
-            else:
-                print("You reach into your satched for a potion. Your fingers grasp air")
-                print(f"The {enemy_encountered['name']} takes advantage of your distraction")
-        elif battle_action == "5":
-            print("You flee successfully!")
-            in_fight = False
+    # Quest List for the current zone
+    quest_list = quests["Fairmeadow"]
+
+    random_index = random.randint(0,len(quests["Fairmeadow"])-1)
+
+    # we need to use the copy() method so we don't change the original
+    chosen_quest = quest_list[random_index].copy()
+    quest_name = chosen_quest['name']
+    quest_list.remove(chosen_quest)
+    
+    in_fight = True #boolean that keeps us in the fight loop
+    print(Fore.BLUE+ "Could be...")
+    sleep(1.5)
+    print(Fore.LIGHTMAGENTA_EX+f"{chosen_quest['backstory']}")
+    quest_accepted = input(Fore.MAGENTA+f"Accept Quest: {quest_name}? (y/n) > ")
+
+    if quest_accepted.lower() == 'y':
+        pass
+    # while in_fight:
+        
+    #     enemy_attack_modifier = 1
+    #     print(f"You have {h_hp} health points remaining \n The {enemy_encountered['name']} has {enemy_encountered['hp']}")
+    #     print(f"1. Attack using {h_wpn}")
+    #     print(f"2. Drink a health potion. ({health_potion_count} in your satchel)")
+    #     print("3. Defend")
+    #     print("4. Do a little dance")
+    #     print("5. Run away")
+    #     battle_action = input("What would you like to do? >")
+    # # Hero's turn
+    #     if battle_action == "1":
+    #         # user has chosen to attach
+    #         # take the hero's attack power - monster'd defense
+    #         # reduce the monster's hp by this calcuation
+    #         damage_dealt = h_atk_pow - enemy_encountered['defense']
+    #         print(Fore.GREEN + f"You strike the {enemy_encountered['name']} for {damage_dealt} damage")
+    #         enemy_encountered['hp'] -= damage_dealt
+    #         # Enemy's turn, if alive
+    #         if enemy_encountered['hp'] > 0:
+    #             print("Your foe staggers but remains in the fight")
+    #             damage_dealt = (enemy_encountered['attack_power'] * enemy_attack_modifier) - the_hero['defense']
+    #             print(f"The {enemy_encountered['name']} strikes for {damage_dealt} damage")
+    #             h_hp -= damage_dealt
+
+    #         else:
+    #             print("Your blade crunches through flesh and bone. The beast lies still and does not rise")
+    #             h_xp += enemy_encountered['xp_drop']
+    #             h_gold += enemy_encountered['gold_drop']
+    #             print(f"You earn {enemy_encountered['xp_drop']} experience and collect {enemy_encountered['gold_drop']} gold")
+    #             in_fight = False
+    #     elif battle_action == "2":
+    #         # Drink a potion
+    #         if health_potion_count > 0:
+    #             the_hero['inventory'].remove("health_potion")
+    #             the_hero['hp'] = the_hero['max_hp']
+    #             print("You take a health potion from your satchel and drink it greedily, restoring your lost hp")
+    #         else:
+    #             print("You reach into your satched for a potion. Your fingers grasp air")
+    #             print(f"The {enemy_encountered['name']} takes advantage of your distraction")
+    #     elif battle_action == "5":
+    #         print("You flee successfully!")
+    #         in_fight = False
--- quests.py
+++ quests.py
@@ -6,16 +6,14 @@ quests = {
             "name": "The Widow's Lament",
             "xp_reward": 250,
             "gold_reward": 35,
-            "Backstory": 
-            f"""
-Could {sleep(.15)} be {sleep(.15)}...{sleep(.15)}
-{sleep(1.15)}
+            "backstory": 
+            """
 There's a widow on the outskirts of town what's been 'avin some trouble
-{sleep(1.15)}
+...
 Been talkin' about seeing some crazy things since 'er ol' man passed earlier this season
-{sleep(1.15)}
-She posted a help wanted notice weeks ago. Everyone in town think she's gone daft so noone'll give her the time of day
-{sleep(1.15)}
+...
+She posted a help wanted notice weeks ago. Everyone in town think she's gone daft so noone'll give 'er the time of day
+...
 Daft or not, she's offered good coin. Might be worth yer while to pay 'er a call
             """ 
         }
